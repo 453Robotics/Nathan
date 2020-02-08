bd6c1ef3e849ffadfe481487042511482dce3380
TecJamari <59777726+TecJamari@users.noreply.github.com>
2020-02-08 14:09:42 -0500

    Add files via upload

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,83 +1,113 @@
-/**
- * Phoenix Software License Agreement
- *
- * Copyright (C) Cross The Road Electronics.  All rights
- * reserved.
- * 
- * Cross The Road Electronics (CTRE) licenses to you the right to 
- * use, publish, and distribute copies of CRF (Cross The Road) firmware files (*.crf) and 
- * Phoenix Software API Libraries ONLY when in use with CTR Electronics hardware products
- * as well as the FRC roboRIO when in use in FRC Competition.
- * 
- * THE SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT
- * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
- * LIMITATION, ANY WARRANTY OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT SHALL
- * CROSS THE ROAD ELECTRONICS BE LIABLE FOR ANY INCIDENTAL, SPECIAL, 
- * INDIRECT OR CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF
- * PROCUREMENT OF SUBSTITUTE GOODS, TECHNOLOGY OR SERVICES, ANY CLAIMS
- * BY THIRD PARTIES (INCLUDING BUT NOT LIMITED TO ANY DEFENSE
- * THEREOF), ANY CLAIMS FOR INDEMNITY OR CONTRIBUTION, OR OTHER
- * SIMILAR COSTS, WHETHER ASSERTED ON THE BASIS OF CONTRACT, TORT
- * (INCLUDING NEGLIGENCE), BREACH OF WARRANTY, OR OTHERWISE
- */
-
-/**
- * Description:
- * The CANifier Demo example demonstrates the currently supported features of
- * CANifier board. In This project the CANifier is used to:
- * 1.) Directly control a LED Strip 
- * 2.) Read PWM Inputs 
- * 3.) Ouput PWM Signals for external peripherials (PWM motor controller)
- * 
- * The project incorporates LIDAR and Gamepad for simple LED control.
- * 
- * As defined by the constants, PWM output is generated on Channel 2, Pin 15
- * CANifier has both a 5v, 3.3v supply pin to power up the LEDs and LIDAR
- * 
- * Controls:
- * Button 6: Run TaskAnimateLEDStrip, automate through HSV color wheel
- * Button 5: Run TaskDirectControlLEDStrip, control HSV value based on left joystick position/angle
- * Button 7: Run TaskLIDAR_ControlLEDStrip, control HSV value based on distance detected by LIDAR
- * Left Joystick: Used in both TaskDirectControlLED Strip, where position of joystick represents
- * position on HSV Wheel, and TaskPWMmotorController, to drive motor forward and reverse when 
- * enabled.
- * 
- * Set gamepadPresent in TaskMainLoop.java to true to enable PWM output to PWM Motor Controller
- * 
- * Supported Version:
- * 	- Talon SRX: 4.0
- * 	- Victor SPX: 4.0
- * 	- Pigeon IMU: 4.0
- * 	- CANifier: 4.0
- */
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import com.ctre.phoenix.ILoopable;
-import frc.robot.Platform.*;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Joystick;
 
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
 public class Robot extends TimedRobot {
-	@Override
-	public void robotInit() {
-        /* Factory Default all hardware to minimize unexpected behaviour */
-        Hardware.canifier.configFactoryDefault();   // Not necessary for CANifier use
-	}
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  
+  //Joystick Decleration
+  Joystick Analog = new Joystick(0);
 
-	@Override
-	public void teleopInit() {
-		/* Add each task to the concurrent scheduler */
-		for (ILoopable loop : Tasks.FullList) {
-			Schedulers.PeriodicTasks.add(loop);
-		}
-	}
+  //X and Y values for joystick
+  double xval;
+  double yval;
+ 
 
-	@Override
-	public void teleopPeriodic() {
-		/** Run forever */
+  /**
+   * This function is run when the robot is first started up and should be
+   * used for any initialization code.
+   */
+  @Override
+  public void robotInit() {
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+  }
 
-		/* Process the concurrent scheduler which will process our tasks */
-		Schedulers.PeriodicTasks.process();
-	}
+  /**
+   * This function is called every robot packet, no matter the mode. Use
+   * this for items like diagnostics that you want ran during disabled,
+   * autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before
+   * LiveWindow and SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+  }
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different autonomous modes using the dashboard. The sendable
+   * chooser code works with the Java SmartDashboard. If you prefer the
+   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+   * getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to
+   * the switch structure below with additional strings. If using the
+   * SendableChooser make sure to add them to the chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
+
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
+    }
+  }
+
+  /**
+   * This function is called periodically during operator control.
+   */
+  @Override
+  public void teleopPeriodic() {
+    //Grab Analog X and Y values
+    xval = Analog.getX();
+    yval = Analog.getY();
+
+    SmartDashboard.putNumber("X Value", xval);
+    SmartDashboard.putNumber("Y Value", yval);
+  }
+
+  /**
+   * This function is called periodically during test mode.
+   */
+  @Override
+  public void testPeriodic() {
+  }
 }
